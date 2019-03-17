b9f74c6f99eacfa0dbbbdfb4165f53d249ff6b73
HasuNoHana <zusant22@gmail.com>
2019-03-17 20:19:14 +0100

    First commit Clion

---
+++ src/Station.cpp
@@ -0,0 +1,57 @@
+#include "Station.h"
+#include "Point.h"
+
+
+
+Station::Station(int name, int people, Point point)
+{
+    this->name = name;
+    this->people = people;
+    this->point.setX( point.getX() );
+    this->point.setY( point.getY() );
+}
+
+Station::Station(int name, int people, int x, int y)
+{
+    this.name = name;
+    this.people = people;
+    this.point.setX( x )
+    this.point.setY( y );
+}
+
+void Station::setName(int newName)
+{
+    name = newName;
+}
+
+int Station::getName()
+{
+    return name;
+}
+
+void Station::setPeople(int newPeople)
+{
+    people = newPeople;
+}
+
+int Station::getPeople()
+{
+    return people;
+}
+
+void Station::setPoint(Point newPoint)
+{
+    this.point.setX( newPoint.getX() );
+    this.point.setY( newPoint.getY() );
+}
+
+void Station::setPoint(int newX, int newY)
+{
+    this.point.setX( newX );
+    this.point.setY( newY );
+}
+
+Point Station::getPoint()
+{
+    return point;
+}
