ac6fbc2380010edee1c3a94ed9cc0e5dfcaca5f7
Tocha10101 <anton.masiukiewicz1@gmail.com>
2019-03-28 18:33:57 +0100

    Add files via upload
    Wersja Pointów z 14.03.2019

---
+++ Point.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "Point.h"
+
+// Constructors
+Point::Point(int x, int y, int stationIndex) {
+    mX = x;
+    mY = y;
+    mStationIndex = stationIndex;
+}
+
+Point::Point() {
+    mX = -1;
+    mY = -1;
+    mStationIndex = -1;
+}
+
+
+// Setters and Getters
+void Point::setX(int newX) {
+    mX = newX;
+}
+
+void Point::setY(int newY) {
+    mY = newY;
+}
+
+int Point::getX() {
+    return mX;
+}
+
+int Point::getY() {
+
+    return mY;
+}
+
+// void Point::setStationIndex(int newStationIndex) {
+//     mStationIndex = newStationIndex;
+// }
+
+// int getStationIndex() {
+//     return mStationIndex;
+// }
+
