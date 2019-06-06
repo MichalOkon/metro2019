8dfb91e4644379260281eb8839e3891b3e690ada
HasuNoHana <zusant22@gmail.com>
2019-06-06 18:22:08 +0200

    Statistics ma 4 zapytania

---
+++ Statistics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zsantorowska on 05.06.19.
+//
+
+#include "Statistics.h"
---
+++ Statistics.h
@@ -0,0 +1,14 @@
+//
+// Created by zsantorowska on 05.06.19.
+//
+
+#ifndef METRO2019_STATISTICS_H
+#define METRO2019_STATISTICS_H
+
+
+class Statistics {
+
+};
+
+
+#endif //METRO2019_STATISTICS_H
